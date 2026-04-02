ac30d3590925e85644e54ddca36fad0326eadcf4
dhanushr2615 <rdhanush335@gmail.com>
2026-04-02 17:03:44 +0530

    corrections

--- vars/smartBuild.groovy
+++ vars/smartBuild.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:]) {
 	echo "Starting the Build ${config.imageName}.."
-	sh "docker build -t ${config.imageName}:${env.BUILD_ID} ."
+	sh "docker build -t ${config.imageName}:${env.BUILD_ID} ./application"
 	echo "Build Completed...!!"
 }
 
