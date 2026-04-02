840db1734b68932d405aecab5b36eb71eff19586
Dhanush <rdhanush335@gmail.com>
2026-04-02 15:25:57 +0530

    Update smartBuild.groovy

--- vars/smartBuild.groovy
+++ vars/smartBuild.groovy
@@ -1,4 +1,4 @@
-def call(Map config) {
+def call(Map config = [:]) {
 	echo "Starting the Build ${config.imageName}.."
 	sh "docker build -t ${condif.imageName}:${env.BUILD_ID} ."
 	echo "Build Completed...!!"
