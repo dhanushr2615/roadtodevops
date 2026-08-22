634924e290f68c9e84180c79ab8e5029602a91cc
dhanushr2615 <rdhanush335@gmail.com>
2026-08-22 12:06:11 +0530

    shell scripts for DevOps

---
+++ sh_scripts/report.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash      #This shebang is most commonly used by DevOps and SRE as this is compatible on all distros
+
+set -euo pipefail   #makes script fail critical for devops
+
+echo "starting upgrade ..."
+
+echo "the value is : $Undefined_var"
+
+grep "ERROR" missingfile.txt | sort | uniq
+
+echo "Script Finished!"
+
+
